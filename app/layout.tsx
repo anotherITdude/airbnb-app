fa606ffa353a336a89dd79bfa44687dd16dcfc61
anotherITdude <abhi.baidya@gmail.com>
2023-04-18 15:26:01 +0200

    Update README.md

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from 'next/font/google'
 
 export const metadata = {
   title: 'AnotherItDude | Airbnb App',
-  description: 'Airbnb app using NextJS, Typescript, Tailwind CSS and Google Authentication using next-auth',
+  description: 'Airbnb app using NextJS, Typescript, Tailwind CSS and Authentication using next-auth',
 }
 
 const font = Nunito({
